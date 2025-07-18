cdbe2995a84af8eb75f13bf736e46eb7c9b6ba7a
JUSTINHARRY <justinharryjayd@gmail.com>
2025-07-18 13:55:59 +0100

    feature: add responsive footer
    - add vite-plugin-svgr dev dependency to enable importing
      SVGs as react components

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,57 @@
+import { List, Text, Flex, Link, HStack, Box } from "@chakra-ui/react";
+import QzBrandText from "../../assets/qz-brand-text.svg?react";
+
+interface WhiteLinkProps {
+	children: React.ReactNode;
+}
+
+const WhiteLink = ({ children }: WhiteLinkProps) => (
+	<Link color="white">{children}</Link>
+);
+
+const Footer = () => (
+	<HStack
+		as="footer"
+		width="100%"
+		mt="24"
+		py={{ base: "4", sm: "8" }}
+		px={{ base: "8", sm: "12" }}
+		bg="blue.600"
+		gap="5"
+		color="white"
+		position="relative"
+		wrap="wrap"
+	>
+		<Flex
+			asChild
+			gap="1"
+			justifyContent="space-between"
+			direction={{ base: "column", sm: "row" }}
+			alignSelf={{ base: "flex-start", sm: "center" }}
+			width={{ base: "35%", sm: "70%" }}
+			mx={{ sm: "auto" }}
+			grow={{ base: "1", sm: "0" }}
+		>
+			<List.Root variant="plain">
+				<List.Item>
+					<WhiteLink>About Us</WhiteLink>
+				</List.Item>
+				<List.Item>
+					<WhiteLink>Contact</WhiteLink>
+				</List.Item>
+				<List.Item>
+					<WhiteLink>Privacy Policy</WhiteLink>
+				</List.Item>
+				<List.Item>
+					<WhiteLink>Terms of Service</WhiteLink>
+				</List.Item>
+			</List.Root>
+		</Flex>
+		<Box width="200px">
+			<QzBrandText style={{ width: "100%", height: "100%" }} />
+		</Box>
+		<Text mx="auto">&copy; QZ Trivia 2025. All rights reserved.</Text>
+	</HStack>
+);
+
+export default Footer;
--- src/components/ui/Layout.tsx
+++ src/components/ui/Layout.tsx
@@ -1,15 +1,19 @@
 import { Outlet } from "react-router";
-import { Box } from "@chakra-ui/react";
+import { Box, Stack } from "@chakra-ui/react";
 
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 const Layout = () => (
-  <Box minH="100vh" bg="gray.100">
-    <NavBar />
-    <Box as="main" pt="7" mx={{ base: "5", md: "15%" }}>
-    	<Outlet />
-    </Box>
-  </Box>
+	<Stack minH="100vh" bg="gray.100">
+		<NavBar />
+		<Box as="main" pt="7" mx={{ base: "5", md: "15%" }} minH="90vh">
+			<Outlet />
+		</Box>
+		<Box mt="auto">
+			<Footer />
+		</Box>
+	</Stack>
 );
 
 export default Layout;

35a1d8cbe5eeef965d050f0c6d72f3fe88be726b
JUSTINHARRY <justinharryjayd@gmail.com>
2025-07-27 16:12:06 +0100

    feature: remove 'results' from header navigation
    - improve Navbar styling

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -4,15 +4,25 @@ import Avatar from "./Avatar";
 import qzBrandText from "../../assets/qz-brand-text.svg";
 
 const NavBar = () => (
-  <HStack as="header" px={{ base: "2", md: "8" }} py="3" bg="gray.200" gap="4">
-    <Link href="/">
-      <Image src={qzBrandText} alt="qz trivia log" width="170px" />
-    </Link>
-    <HStack justifyContent="space-between" flexGrow="1">
-      <Navigation />
-      <Avatar />
-    </HStack>
-  </HStack>
+	<HStack
+		as="header"
+		px={{ base: "2", md: "8" }}
+		py="3"
+		gap="4"
+		justifyContent="space-between"
+		borderBottomWidth="2px"
+		borderBottomColor="gray.200"
+	>
+		<Link href="/">
+			<Image src={qzBrandText} alt="qz trivia log" width="170px" />
+		</Link>
+		<HStack gap="5">
+			<Navigation />
+			<Avatar />
+		</HStack>
+	</HStack>
 );
+
+// flexGrow="1"
 
 export default NavBar;
--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -1,10 +1,19 @@
-import { HStack, Link } from "@chakra-ui/react";
+import { HStack, Button } from "@chakra-ui/react";
+import { Link } from "react-router";
 
 const Navigation = () => (
-  <HStack as="nav" fontWeight="bold" gap="7" fontSize="sm">
-    <Link href="/">Home</Link>
-    <Link>Results</Link>
-  </HStack>
+	<HStack as="nav" fontWeight="bold" gap="7" fontSize="sm">
+		<Button
+			asChild
+			fontWeight="bold"
+			variant="ghost"
+			rounded="full"
+			px={{ base: "1", md: "4" }}
+			_hover={{ color: "white", bg: "blue.600" }}
+		>
+			<Link to="/">Home</Link>
+		</Button>
+	</HStack>
 );
 
 export default Navigation;
